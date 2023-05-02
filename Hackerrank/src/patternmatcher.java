3875ed61473ef27a48e619971d7f87acc95c08b6
Anilcss <anilkj8@gmail.com>
2023-05-02 22:51:50 +0530

    HAckerrank2

--- Hackerrank/src/patternmatcher.java
+++ Hackerrank/src/patternmatcher.java
@@ -6,15 +6,42 @@ public class patternmatcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the value to search");
-		String s=sc.next();
-		Pattern pt=Pattern.compile("[^a-z]",Pattern.CANON_EQ);
-		Matcher mt=pt.matcher(s);
-		boolean found=mt.find();
-		System.out.println("New ");
-		System.out.println(found);
+		String s = sc.next();
+		char[] ch = s.toCharArray();
+		Pattern small = Pattern.compile("[a-z]", Pattern.CANON_EQ);
+		Pattern caps=Pattern.compile("[A-Z]",Pattern.CANON_EQ);
+		int smallletter = 0, capsletter = 0, number = 0, specialchar = 0;
+		for (int i = 0; i < s.length(); i++) {
+				String sm1,sm2,sm3=null;
+				sm1=Character.toString(ch[i]);
+				sm2=Character.toString(ch[i]);
+			Matcher mt = small.matcher(sm1);
+			Matcher cp=caps.matcher(sm2);
+			boolean sm = mt.find();
+			boolean cap1=cp.find();
+			if (sm == true) {smallletter++;} 
+			if (cap1==true) {capsletter++;}
+			
+			
+			
+		}
+
+		System.out.println("Small letter  "+smallletter);
+		System.out.println("Caps letter  "+capsletter);
 		
+		
+	System.out.println(valid(smallletter,capsletter));
+
 	}
+private static boolean valid(int smallletter,int capsletter) {
+	
+	if(smallletter>=1 && capsletter>=1)
+	{
+		return true;
+	}
+	return false;
+}
 
 }
